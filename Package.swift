53825c6fa6e22dffb2e4fdab60b7bc703969859d
1234 <a1234@Astons-MacBook-Pro.local>
2025-11-25 22:57:58 +0800

    优化SDK

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Analytics_Facebook",
-            url: "https://github.com/HiZeusai/SDKPackage/releases/download/2.1.6/Analytics_Facebook.xcframework.zip",
-            checksum: "4e21b0fb4af0ec995a641e3ab9ec5b58441b15045c2c1f32a50befe6555e8d55"
+            url: "https://github.com/HiZeusai/SDKPackage/releases/download/2.1.8/Analytics_Facebook_2.1.8.zip",
+            checksum: "42bcbd5e0f658be437deab8dc4290130cf90aa275799e4afce42623c5f6d6a25"
         ),
         .target(
             name: "AnalyticsFacebookWrapper",
